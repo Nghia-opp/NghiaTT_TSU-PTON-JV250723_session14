ac4c7dac28d832e33fcae3862b9188c74775e1fd
Nghia-opp <nt14499@gmail.com>
2025-08-27 00:16:39 +0900

    Upload bài tập SS14 bai02

---
+++ bai02/index.js
@@ -0,0 +1,22 @@
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+}
+console.log(car);
+
+car.color = "red";
+
+console.log(car);
+
+car.year = 2022;
+
+console.log(car);
+
+alert(`đối tượng car với các thuộc tính sau:
+    brand: ${car.brand}
+    model: ${car.model}
+    year: ${car.year}
+    color: ${car.color}`);
+
+
